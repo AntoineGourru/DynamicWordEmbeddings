2bdbe6b0846e34dc158b11efe2f07be74daf2cd8
AntoineGourru <antoine.gourru@gmail.com>
2018-12-12 14:39:24 +0100

    BSG V2

--- model.R
+++ model.R
@@ -14,8 +14,19 @@ init_model <- function(D,K,sigma_t,sigma_0,nb_epochs,nb_MB,nb_sampleVI){
   model$U <- list()
   model$V <- list()
   
+  model$vp_m1 <- draw_infoless_VP(D,K)  
+  
   model$timePosition <- 1
   return(model)
+}
+
+draw_infoless_VP <- function(D,K){
+  vp <- list()
+  vp$u_mu <- matrix(0,D,K)
+  vp$v_mu <- matrix(0,D,K)
+  vp$u_sigma <- matrix(1,D,K)
+  vp$v_sigma <- matrix(1,D,K)
+  return(vp)
 }
 
 
