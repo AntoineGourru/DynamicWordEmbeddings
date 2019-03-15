02318950afd6e35f6f75f5f4f3b32c4d88fdc56f
AntoineGourru <antoine.gourru@gmail.com>
2019-03-15 12:59:23 +0100

    barkan

---
+++ optimizationBarkan.R
@@ -0,0 +1,160 @@
+optimi <- function(X_t,model,color){
+  logLout <- c()
+  
+  vp <- draw_VP_withoutSigma(model) 
+  
+  Ut <- vp$u_mu
+  Vt <- vp$v_mu
+  
+  plot(Ut,col = color)
+  logL <- likely(X_t,Ut,Vt)
+  print(logL)
+  
+  C <- X_t$P - X_t$N 
+  
+  
+  for (epo in 1:model$nb_epochs) {
+    print(paste("eopch :",epo))
+    
+    #XI
+    
+    for (i in 1:model$D) {
+      for (j in 1:model$D) {
+        A <- (vp$u_sigma[i,] * vp$u_sigma[i,] + vp$u_mu[i,] * vp$u_mu[i,]) * (vp$v_sigma[j,]*vp$v_sigma[j,] + vp$v_mu[j,]*vp$v_mu[j,])
+        vp$xi[i,j] <- sqrt(sum(A))
+        # print(vp$xi[i,j])
+      }
+    }
+    
+    for (i in 1:model$D) {
+      # R
+      r_ui <- colSums(C[i,] * vp$v_mu)
+      
+      #P
+      P <- matrix(0,model$K,model$K)
+      for (j in 1:model$D) {
+        E <- diag(vp$v_sigma[j,]) + vp$v_mu[j,] %*% t(vp$v_mu[j,])
+        sig_xi <- 1/(1 + exp(-vp$xi[i,j]))
+        deux_lambda_xi <- (1/sig_xi)  * (sig_xi  - 0.5)
+        
+        P <- P + (deux_lambda_xi *  E)
+        
+      }
+      P <- P + diag(rep(1,model$K))
+      
+      Pm1 <- solve(P)
+      vp$u_mu[i,] <- Pm1 %*% r_ui
+      
+      vp$u_sigma[i,] <- diag(Pm1)
+      
+    }
+    
+    for (i in 1:model$D) {
+      # R
+      r_vi <- colSums(C[,i] * vp$u_mu)
+      
+      #P
+      P <- matrix(0,model$K,model$K)
+      for (j in 1:model$D) {
+        E <- diag(vp$u_sigma[j,]) + vp$u_mu[j,] %*% t(vp$u_mu[j,])
+        sig_xi <- 1/(1 + exp(-vp$xi[j,i]))
+        deux_lambda_xi <- (1/sig_xi)  * (sig_xi  - 0.5)
+        
+        P <- P + (deux_lambda_xi *  E)
+        
+      }
+      P <- P + diag(rep(1,model$K))
+      
+      Pm1 <- solve(P)
+      vp$v_mu[i,] <- Pm1 %*% r_vi
+      
+      vp$v_sigma[i,] <- diag(Pm1)
+      
+    }
+    
+    Ut <- vp$u_mu
+    Vt <- vp$v_mu
+    
+    logL <- likely(X_t,Ut,Vt)
+    print(logL)
+    logLout <- c(logLout,logL)
+    plot(Ut,col = color)
+  } # epo
+  
+  model$timePosition <- model$timePosition + 1 
+  return(list(embedding = Ut,ll=logLout,sigma = vp$u_sigma))
+}
+
+draw_VP_withoutSigma <- function(model){
+  vp <- list()
+  vp$u_mu <- mvrnorm(n = model$D, rep(0,model$K),diag(rep(1,model$K)))
+  vp$v_mu <- mvrnorm(n = model$D, rep(0,model$K),diag(rep(1,model$K)))
+  vp$u_sigma <- matrix(1,model$D,model$K)
+  vp$v_sigma <- matrix(1,model$D,model$K)
+  vp$xi <- matrix(0,model$D,model$D)
+  return(vp)
+}
+
+draw_VP <- function(model){
+  vp <- list()
+  vp$u_mu <- mvrnorm(n = model$D, rep(0,model$K),diag(rep(1,model$K)))
+  vp$v_mu <- mvrnorm(n = model$D, rep(0,model$K),diag(rep(1,model$K)))
+  vp$u_sigma <- abs(mvrnorm(n = model$D, rep(0,model$K),diag(rep(1,model$K))))
+  vp$v_sigma <- abs(mvrnorm(n = model$D, rep(0,model$K),diag(rep(1,model$K))))
+  return(vp)
+}
+
+likely <- function(X_t,U,V){
+  
+  x <- U %*% t(V)
+  
+  sig <- -log(1 + exp(-x))
+  
+  gauche <- X_t$P * sig
+  
+  sig <- -log(1 + exp(x))
+  
+  droite <- X_t$N * sig
+  
+  ll <- sum(gauche + droite)
+  
+  # ll <- ll +  log(dmvnorm(U[i,],rep(0,model$K),diag(rep(1,model$K))))
+  # ll <- ll +  log(dmvnorm(V[i,],rep(0,model$K),diag(rep(1,model$K))))
+  return(ll)
+}
+
+
+
+source("model.R")
+source("utils.R")
+# (D,K,sigma_t,sigma_0,nb_epochs,nb_MB,nb_sampleVI)
+n <- 300
+
+model <- init_model(n,2,1,1,nb_epochs = 100,3,500)
+model$timePosition
+X_t <- list()
+
+# GenerateData
+
+K = 3
+alpha = rep(1/K, K)
+intra <- 0.8
+inter <- (1 - intra)/(K-1)
+pi = matrix(inter, K, K)
+diag(pi) = intra
+e <- n * 100
+rSBM <- randomSBM(n,e,K,alpha,pi)
+
+# rSBM$Adj
+# rSBM$cluster
+
+
+X_t$P <- as.matrix(rSBM$Adj)
+X_t$N <- 4 * round((rowSums(X_t$P) %*% t(colSums(X_t$P))) / sum(X_t$P))
+# debug(optimi)
+blob <- optimi(X_t,model,rSBM$cluster)
+plot(blob$ll)
+
+
+
+
