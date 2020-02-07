b83275a33e9d6b8017850c85a31919eed1f5695c
AntoineGourru <antoine.gourru@gmail.com>
2020-02-07 12:01:17 +0100

    debug

--- W2V_tensorflowExample.py
+++ W2V_tensorflowExample.py
@@ -7,7 +7,8 @@ import scipy.sparse as sp
 import numpy as np
 import random
 import tensorflow as tf
-tf.enable_eager_execution()
+import logging
+logger = logging.getLogger()
 
 #X  : sp matrice
 #k  : number of negatiev examples
@@ -38,8 +39,8 @@ def cooc_to_list(cooc,val):
 class DWE(Model):
     def __init__(self,v,d,T):
         super(DWE, self).__init__()
-        self.U = layers.Embedding(v,d,embeddings_initializer = RandomUniform(minval=-2, maxval=2))
-        self.V = layers.Embedding(v,d,embeddings_initializer = RandomUniform(minval=-2, maxval=2))
+        self.U = layers.Embedding(v,d)
+        self.V = layers.Embedding(v,d)
         
     def call(self, pair):
         i,j,de = tf.split(pair, [1,1,1], 1)
@@ -49,7 +50,7 @@ class DWE(Model):
         x = tf.reduce_sum(tf.multiply(u, v),axis = 1)
         x = tf.expand_dims(x,1)
         x = tf.multiply(de,x)
-        x = tf.math.log(tf.math.sigmoid(x))
+        x = tf.math.sigmoid(x)
         return -x
     
 @tf.function
@@ -89,42 +90,44 @@ voc2id = dict(zip(voc,list(range(len(voc)))))
 cooc = inpu['data']
 years = inpu['years']
 
-cooc = sum([*cooc.values()])
-#cooc = sum([cooc[12],cooc[13],cooc[14]])
+#cooc = sum([*cooc.values()])
+cooc = sum([cooc[12],cooc[13],cooc[14]])
 #cooc = cooc[0]
 
 compute_nn(voc2id["classification"],cooc.todense(),cooc.todense(),voc,5)
 
-print("Vocabulary of size %d with %d observed cooccurences" % (len(voc),np.sum(cooc)))
+
+print("Vocabulary of size %d with %d observed cooccurences" % (len(voc),np.sum(cooc)), flush=True)
 
 X_pos = cooc_to_list(cooc,1)
+X_neg = neg_example(cooc,10)
+X_neg = cooc_to_list(X_neg,-1)
+X = X_pos + X_neg
+data = tf.data.Dataset.from_tensor_slices(np.asarray(X)).shuffle(35000000).batch(1024)
 
 dwe = DWE(len(voc),160,1)
   
 train_loss = tf.keras.metrics.Sum(name='train_loss')
 optimizer = tf.keras.optimizers.Adagrad()
 
-nepochs = 40
+nepochs = 3
 
-print('Starting Learning')
+print('Starting Learning', flush=True)
 ll = []
 for epoch in range(nepochs):
-    X_neg = neg_example(cooc,10)
-    X_neg = cooc_to_list(X_neg,-1)
-    X = X_pos + X_neg
-    data = tf.data.Dataset.from_tensor_slices(np.asarray(X)).shuffle(35000000).batch(1024)
+
     # Reset the metrics at the start of the next epoch
     train_loss.reset_states()
     train(dwe,data,optimizer,train_loss)
     template = 'Epoch {}, Loss: {}'
-    print(template.format(epoch+1,train_loss.result()))
+    print(template.format(epoch+1,train_loss.result()), flush=True)
     ll.append(train_loss.result())
 
 U = np.array(dwe.U(tf.constant(list(range(len(voc))))))
 V = np.array(dwe.V(tf.constant(list(range(len(voc))))))
 compute_nn(voc2id["classification"],U,V,voc,5)
 
-import matplotlib.pyplot as plt
-plt.plot(ll)
-plt.ylabel('loss')
-plt.show()
+#import matplotlib.pyplot as plt
+#plt.plot(ll)
+#plt.ylabel('loss')
+#plt.show()
